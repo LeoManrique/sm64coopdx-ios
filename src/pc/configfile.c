ef1451eed951615f74270067ff389e51e96a536e
Leonardo Manrique <leonardomanrique43@gmail.com>
2026-04-18 16:43:50 +0200

    Mute audio on focus loss by default
    Set configMuteFocusLoss to true by default so when switching apps the game doesn't keep playing the game music. This is not a consistent behavior so no migration for existing users is done.

--- src/pc/configfile.c
+++ src/pc/configfile.c
@@ -108,7 +108,11 @@ unsigned int configMusicVolume                    = MAX_VOLUME;
 unsigned int configSfxVolume                      = MAX_VOLUME;
 unsigned int configEnvVolume                      = MAX_VOLUME;
 bool         configFadeoutDistantSounds           = false;
+#ifdef TARGET_IOS
+bool         configMuteFocusLoss                  = true;
+#else
 bool         configMuteFocusLoss                  = false;
+#endif
 // control binds
 #ifdef TOUCH_CONTROLS
 unsigned int configKeyA[MAX_BINDS]                = { 0x0026,     0x1000,     VK_INVALID };
